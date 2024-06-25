a32bdce52f583d67f79dc20c0475343a2dbb5add
Soumya sankar Dutta <soumyasankardutta@gmail.com>
2024-06-25 16:21:25 +0530

    Angular16 Implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
 })
 export class AppComponent {
-  title = 'FusionChartsAngular16Demo';
+  dataSource: Object;
+  title: string;
+
+  constructor() {
+    this.title = "Angular  FusionCharts Sample";
+
+    this.dataSource = {
+      chart: {
+        caption: "Countries With Most Oil Reserves [2017-18]",
+        subCaption: "In MMbbl = One Million barrels",
+        xAxisName: "Country",
+        yAxisName: "Reserves (MMbbl)",
+        numberSuffix: "K",
+        theme: "fusion",
+      },
+      data: [
+        { label: "Venezuela", value: "290" },
+        { label: "Saudi", value: "260" },
+        { label: "Canada", value: "180" },
+        { label: "Iran", value: "140" },
+        { label: "Russia", value: "115" },
+        { label: "UAE", value: "100" },
+        { label: "US", value: "30" },
+        { label: "China", value: "30" },
+      ],
+    };
+  }
 }
